308efce430514e04ecb236e7f609bf70a8c5be03
Bartek Winnicki <bartlomiejmwinnicki@gmail.com>
2022-10-29 17:20:22 +0200

    Added two WebElement fields, and two methods that will be used in BuyItemTest

--- src/test/java/productStore/pages/ProductPage.java
+++ src/test/java/productStore/pages/ProductPage.java
@@ -1,2 +1,41 @@
-package productStore.pages;public class ProductPage {
+package productStore.pages;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductPage {
+
+    WebDriver driver;
+
+    public ProductPage(WebDriver driver) {
+        PageFactory.initElements(driver,this);
+        this.driver=driver;
+    }
+
+    @FindBy(css = "[class='btn btn-success btn-lg']")
+    private WebElement addToCartBtn;
+
+    @FindBy(css = "#cartur")
+    private WebElement cartLink;
+
+    private static final Logger logger = LogManager.getLogger();
+
+    public void addToCart() throws InterruptedException {
+        logger.info("Adding product to cart");
+        Thread.sleep(2000);
+        addToCartBtn.click();
+        logger.info("Product added to cart");
+        Thread.sleep(1000);
+        driver.switchTo().alert().accept();
+    }
+
+    public void goToCart() {
+        logger.info("Going to cart");
+        cartLink.click();
+    }
+
 }
--- src/test/java/productStore/tests/BuyItemTest.java
+++ src/test/java/productStore/tests/BuyItemTest.java
@@ -1,2 +1,20 @@
-package productStore.tests;public class BuyItemTest {
+package productStore.tests;
+
+import org.testng.annotations.Test;
+import productStore.pages.HomePage;
+import productStore.pages.ProductPage;
+
+public class BuyItemTest extends BaseTest {
+
+    @Test
+    public void buyItem() throws InterruptedException {
+        HomePage homePage = new HomePage(driver);
+        Thread.sleep(2000);
+        homePage.clickProductImage();
+        ProductPage productPage = new ProductPage(driver);
+        productPage.addToCart();
+        Thread.sleep(2000);
+        productPage.goToCart();
+        Thread.sleep(3000);
+    }
 }
