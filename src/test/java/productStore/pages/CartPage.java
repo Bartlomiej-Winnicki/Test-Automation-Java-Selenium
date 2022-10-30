af44937adff4a86272b167d131ed0fd41f44e732
Bartek Winnicki <bartlomiejmwinnicki@gmail.com>
2022-10-30 11:01:08 +0100

    Added buyItemTest and necessary Webelement fields and methods

--- src/test/java/productStore/pages/CartPage.java
+++ src/test/java/productStore/pages/CartPage.java
@@ -1,2 +1,89 @@
-package productStore.pages;public class CartPage {
+package productStore.pages;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class CartPage {
+
+    WebDriver driver;
+
+    public CartPage(WebDriver driver) {
+        PageFactory.initElements(driver,this);
+        this.driver=driver;
+    }
+
+    @FindBy(xpath = "//button[@data-target='#orderModal']")
+    private WebElement placeOrderBtn;
+
+    @FindBy(css = "#name")
+    private WebElement nameInput;
+
+    @FindBy(css = "#country")
+    private WebElement countryInput;
+
+    @FindBy(css = "#city")
+    private WebElement cityInput;
+
+    @FindBy(css = "#card")
+    private WebElement cardInput;
+
+    @FindBy(css = "#month")
+    private WebElement monthInput;
+
+    @FindBy(css = "#year")
+    private WebElement yearInput;
+
+    @FindBy(xpath = "//button[@onclick='purchaseOrder()']")
+    private WebElement purchaseBtn;
+
+    private static final Logger logger = LogManager.getLogger();
+
+    public void clickPlaceOrderButton() {
+        logger.info("Clicking 'Place Order' button");
+        placeOrderBtn.click();
+    }
+
+    public void enterName(String name) {
+        logger.info("Entering name");
+        nameInput.sendKeys(name);
+    }
+
+    public void enterCountry(String country) {
+        logger.info("Entering country");
+        countryInput.sendKeys(country);
+    }
+
+    public void enterCity(String city) {
+        logger.info("Entering city");
+        cityInput.sendKeys(city);
+    }
+
+    public void enterCardNumber(String cardNumber) {
+        logger.info("Entering card number");
+        cardInput.sendKeys(cardNumber);
+    }
+
+    public void enterMonth(String month) {
+        logger.info("Entering month number");
+        monthInput.sendKeys(month);
+    }
+
+    public void enterYear(String year) {
+        logger.info("Entering year");
+        yearInput.sendKeys(year);
+    }
+
+    public void clickPurchaceBtn() {
+        logger.info("Clicking purchase button");
+        purchaseBtn.click();
+    }
+
+
+
+
 }
--- src/test/java/productStore/tests/BuyItemTest.java
+++ src/test/java/productStore/tests/BuyItemTest.java
@@ -1,13 +1,14 @@
 package productStore.tests;
 
 import org.testng.annotations.Test;
+import productStore.pages.CartPage;
 import productStore.pages.HomePage;
 import productStore.pages.ProductPage;
 
 public class BuyItemTest extends BaseTest {
 
     @Test
-    public void buyItem() throws InterruptedException {
+    public void buyItemTest() throws InterruptedException {
         HomePage homePage = new HomePage(driver);
         Thread.sleep(2000);
         homePage.clickProductImage();
@@ -15,6 +16,17 @@ public class BuyItemTest extends BaseTest {
         productPage.addToCart();
         Thread.sleep(2000);
         productPage.goToCart();
+        Thread.sleep(3000);
+        CartPage cartPage = new CartPage(driver);
+        cartPage.clickPlaceOrderButton();
+        Thread.sleep(2000);
+        cartPage.enterName("Bartłomiej Winnicki");
+        cartPage.enterCountry("Poland");
+        cartPage.enterCity("Opole");
+        cartPage.enterCardNumber("1111111111111");
+        cartPage.enterMonth("12");
+        cartPage.enterYear("1986");
+        cartPage.clickPurchaceBtn();
         Thread.sleep(3000);
     }
 }
